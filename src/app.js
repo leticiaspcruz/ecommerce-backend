bb011604aa3a21b8873f0a5318f8e9accd64764a
leticiaspcruz <leticiaspcruz@gmail.com>
2023-08-05 19:23:18 -0300

    feat: adding github passport and views

--- src/app.js
+++ src/app.js
@@ -2,13 +2,16 @@ import express, { urlencoded } from 'express';
 import path from 'path';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-// import session from "express-session";
-// import cookieParser from 'cookie-parser';
-// import MongoStore from "connect-mongo";
+import session from "express-session";
+import cookieParser from 'cookie-parser';
+import MongoStore from "connect-mongo";
 import productRoutes from './routes/productRoutes.js';
 import cartRoutes from './routes/cartRoutes.js';
 import messagesRoutes from './routes/messageRoutes.js';
-// import initializePassport from "./config/passportConfig.js";
+import viewsRoutes from './routes/viewsRoutes.js';
+import loginRoutes from './routes/loginRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import initializePassport from "./config/passportConfig.js";
 import  handlebars  from 'express-handlebars';
 
 const app = express();
@@ -27,29 +30,29 @@ mongoose.connect(mongoAtlasUrl).then(() => console.log("Connected to mongoDB"))
     }
 });
 
-// initializePassport();
+initializePassport();
 
-const viewFolder = './dao/views';
+const viewFolder = 'src/dao/views';
 app.engine('handlebars', handlebars.engine());
 app.set('views', viewFolder);
 app.set('view engine', 'handlebars');
 
 app.use(urlencoded({extended: true}));
 app.use(express.json());
-// app.use(cookieParser());
-// app.use(session({
-//     store: MongoStore.create({
-//         mongoUrl: mongoAtlasUrl,
-//         mongoOptions: { useUnifiedTopology: true },
-//         ttl: 150,
-//     }),
-//     secret: process.env.MONGODB_SECRET,
-//     resave: false,
-//     saveUninitialized: true
-// }));
-
-// app.use('/api/login', loginRouter);
-// app.use('/api/user', userRouter);
+app.use(cookieParser());
+app.use(session({
+    store: MongoStore.create({
+        mongoUrl: mongoAtlasUrl,
+        mongoOptions: { useUnifiedTopology: true },
+        ttl: 150,
+    }),
+    secret: process.env.MONGODB_SECRET,
+    resave: false,
+    saveUninitialized: true
+}));
+app.use('/', viewsRoutes);
+app.use('/api/login', loginRoutes);
+app.use('/api/user', userRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/carts', cartRoutes);
 app.use('/api/messages', messagesRoutes);

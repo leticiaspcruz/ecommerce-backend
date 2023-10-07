481afde9cd5ca574c1a73e09824eb81aedaefa83
leticiaspcruz <leticiaspcruz@gmail.com>
2023-10-07 18:37:02 -0300

    feat: add nodemailer and twilio sms

--- src/dao/routes/index.js
+++ src/dao/routes/index.js
@@ -1,12 +1,24 @@
 import { Router } from 'express';
 import passport from 'passport';
+import nodemailer from 'nodemailer';
+import twilio from 'twilio';
 import ProductController from '../../controllers/productController.js';
 import CartController from '../../controllers/cartController.js';
 import MessageController from '../../controllers/messageController.js';
 import UserController from '../../controllers/userController.js';
+import { 
+    NODEMAILER_CONFIG, 
+    TWILIO_ACCOUNT_SID, 
+    TWILIO_AUTH_TOKEN, 
+    TWILIO_SMS_NUMBER 
+} from '../../constants.js';
+
+const transport = nodemailer.createTransport(NODEMAILER_CONFIG);
 
 const routes = Router();
 const authenticate = passport.authenticate('jwt', { session: false });
+const twilioClient = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+
 
 routes.get('/', (req, res) => {
     res.send('Bem vindx!')
@@ -37,5 +49,34 @@ routes.post('/api/login', UserController.userLogin);
 routes.get('/api/users', authenticate, UserController.getUsers);
 routes.get('/api/users/:userId', UserController.getUserById);
 
+//mailing routes
+routes.post('/api/email', (req, res) => {
+    let result = transport.sendMail({
+        from: 'leticiacoderhouse@gmail.com',
+        to: 'leticiacoderhouse@gmail.com',
+        subject: 'Teste de envio de email',
+        html: `
+        <div>
+        <h1>Teste de envio de email</h1>
+        </div>`,
+    });
+
+    res.send('Email enviado com sucesso!');
+});
+
+routes.post('/api/sms', (req, res) => {
+    const {
+        name,
+        orderNumber,
+        phoneNumber
+    } = req.body;
+    const result = twilioClient.messages.create({
+        body: `Olá ${name}, seu pedido ${orderNumber} foi recebido com sucesso! `,
+        from: TWILIO_SMS_NUMBER,
+        to: phoneNumber
+    });
+    console.log(`SMS enviado para ${phoneNumber}`);
+    return res.send(`SMS enviado para ${phoneNumber} : ${name}  - Order ${orderNumber}`);
+});
 
 export default routes;

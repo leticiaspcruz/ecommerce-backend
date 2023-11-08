668558502d9299e9f753fc3fb28d48b4e8c1400c
leticiaspcruz <leticiaspcruz@gmail.com>
2023-11-08 20:53:59 -0300

    adjust at login

--- src/dao/models/userSchema.js
+++ src/dao/models/userSchema.js
@@ -1,6 +1,9 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: './.env'});
 
 const userCollection = 'users';
 const userSchema = new mongoose.Schema({
@@ -18,7 +21,7 @@ userSchema.methods.comparePassword = async function(candidatePassword) {
 };
 
 userSchema.methods.generateAuthToken = function() {
-  const token = jwt.sign({ _id: this._id }, 'your-secret-key', { expiresIn: '1h' }); // Troque 'your-secret-key' pela sua chave secreta
+  const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET_KEY, { expiresIn: '30d' });
   return token;
 };
 
